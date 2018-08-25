371e2204d58518cc60aae5fe55ce7791191a52da
Sandeep Adwankar <sandeepadwankar@sandeep.local>
2018-08-24 19:59:36 -0700

    6.6

--- stocks-grid/src/StocksGrid.js
+++ stocks-grid/src/StocksGrid.js
@@ -8,11 +8,29 @@ export default class StocksGrid extends Component {
 
     constructor() {
         super();
+        this.store = new Ext.data.Store({
+            data: stocks,
+            autoLoad: true,
+            sorters: [{
+                property: 'name'
+            }],
+            listeners: {
+                update: this.onRecordUpdated
+            }
+        })
+    }
+
+    onRecordUpdated = (store, record, operation, modifiedFieldNames) => {
+        const field = modifiedFieldNames[0];
+        Ext.toast(`${record.get('name')} ${field} updated to ${record.get(field)}`)
     }
 
     render() {
         return (
-           <Grid>
+           <Grid
+                ref={grid => this.grid = grid} 
+                store={this.store}
+           >
                 <Column dataIndex="name" text="Name" width={300} />
                 <Column dataIndex="symbol" text="Symbol" />
                 <Column dataIndex="ticks" text="Trend" />
