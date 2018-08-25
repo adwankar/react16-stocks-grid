055f0be6d0ea8a1b65dee01adc056bea0846a346
Sandeep Adwankar <sandeepadwankar@sandeep.local>
2018-08-24 20:15:51 -0700

    6.6

--- stocks-grid/src/StocksGrid.js
+++ stocks-grid/src/StocksGrid.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { ExtReact,Container } from '@sencha/ext-react';
-import { Grid, Column } from '@sencha/ext-react';
+import { ExtReact, Container } from '@sencha/ext-react';
+import { Grid, Column, Button, WidgetCell } from '@sencha/ext-modern';
 import stocks from '../data/stocks';
 
 
@@ -20,22 +20,35 @@ export default class StocksGrid extends Component {
         })
     }
 
+    buyHandler = (button) => {
+        let gridrow = button.up('gridrow'),
+        record = gridrow.getRecord();
+        Ext.toast(`Buy ${record.get('name')}`)
+    }
+
     onRecordUpdated = (store, record, operation, modifiedFieldNames) => {
         const field = modifiedFieldNames[0];
         Ext.toast(`${record.get('name')} ${field} updated to ${record.get(field)}`)
     }
 
+
     render() {
         return (
-           <Grid
-                ref={grid => this.grid = grid} 
+            <Grid
+                ref={grid => this.grid = grid}
                 store={this.store}
-           >
-                <Column dataIndex="name" text="Name" width={300} />
+            >
+                <Column >
+                    <WidgetCell>
+                        <Button ui="round action" className="x-item-no-select" handler={this.buyHandler} text="Buy" />
+                    </WidgetCell>
+                </Column>
+                <Column dataIndex="name" text="Name" width={300} cell={{ style: { fontWeight: 'bold' } }} />
                 <Column dataIndex="symbol" text="Symbol" />
                 <Column dataIndex="ticks" text="Trend" />
                 <Column dataIndex="sector" text="Sector" width={200} />
                 <Column dataIndex="industry" text="Industry" width={350} />
+                
             </Grid>
         );
     }
